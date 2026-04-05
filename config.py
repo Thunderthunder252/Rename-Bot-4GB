13cfb69345712b8b453b6f438caa20806cbcdd1f
Thunder <vishnusakinala252@gmail.com>
2026-04-05 12:47:24 +0530

    as

--- config.py
+++ config.py
@@ -1,30 +1,26 @@
 import os
 
+# --- Required Variables Config ---
+# We removed the 'os.environ' part so the bot uses your numbers/text directly
+API_ID = 37674103
+API_HASH = "f9ecc621ed4865256f94f71a7dd6a1c8"
+BOT_TOKEN = "8699789214:AAGJJLwBeTaqItUm3v4zgIObrVaDIcWFrbA"
+ADMIN = 6415053610  # This is your ID from the screenshots
 
+# --- Premium 4GB Renaming Client Config ---
+STRING_SESSION = ""
 
-# Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+# --- Log & Force Channel Config ---
+FORCE_SUBS = ""
+LOG_CHANNEL = 0
 
+# --- Mongo DB Database Config ---
+# IMPORTANT: You still need to paste a MongoDB URL here for thumbnails to work!
+DATABASE_URL = "PASTE_YOUR_MONGODB_URL_HERE"
+DATABASE_NAME = "madflixbotz"
 
-# Premium 4GB Renaming Client Config
-STRING_SESSION = os.environ.get("STRING_SESSION", "")
-
-
-# Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-
-
-# Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
-
-
-# Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+# --- Other Variables Config ---
+START_PIC = "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg"
 
 
 
